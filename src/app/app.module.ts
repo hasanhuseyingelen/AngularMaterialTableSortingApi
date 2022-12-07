f25ff256626dc6872472e57e53ccdf418ca7d821
hasanhuseyingelen <hasanhuseyingelen@gmail.com>
2022-12-07 23:19:48 +0300

    Create usersTable component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersTableModule } from './usersTable/usersTable.module';
+import { UsersTableComponent } from './usersTable/components/users-table/users-table.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersTableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UsersTableModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
